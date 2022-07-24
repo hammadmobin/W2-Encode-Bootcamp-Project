534ea656ea1e14fe4db066324444c871fe5982db
Muhammad Hammad Mobin <muhammadhammadmobin@gmail.com>
2022-07-24 21:29:35 +0500

    Add the QueryProposal Script

---
+++ Project/scripts/queryProposals.ts
@@ -0,0 +1,72 @@
+import { BigNumber, Contract,ethers } from "ethers";
+import "dotenv/config";
+import * as ballotJson from "../artifacts/contracts/CustomBallot.sol/CustomBallot.json";
+import { CustomBallot } from "../typechain";
+
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+
+function convertStringArrayToBytes32(array: string[]) {
+    const bytes32Array = [];
+    for (let index = 0; index < array.length; index++) {
+      bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+    }
+    return bytes32Array;
+  }
+function setupProvider() {
+  const infuraOptions = process.env.INFURA_API_KEY
+    ? process.env.INFURA_API_SECRET
+      ? {
+          projectId: process.env.INFURA_API_KEY,
+          projectSecret: process.env.INFURA_API_SECRET,
+        }
+      : process.env.INFURA_API_KEY
+    : "";
+  const options = {
+    alchemy: process.env.ALCHEMY_API_KEY,
+    infura: infuraOptions,
+  };
+  const provider = ethers.providers.getDefaultProvider("goerli", options);
+  return provider;
+}
+
+
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Using address ${wallet.address}`);
+
+  const provider = setupProvider();
+
+  const signer = wallet.connect(provider);
+
+  const balanceBN = await signer.getBalance();
+
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+
+  console.log(`Wallet balance ${balance}`);
+
+  if (process.argv.length < 3) throw new Error("Ballot address missing");
+  const ballotAddress = process.argv[2];
+
+  const ballotContract: CustomBallot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as CustomBallot;
+
+  for (let i = 0; i < Number(await ballotContract.getProposalsLength()); i++) {
+    var proposal =  await ballotContract.proposals(i);
+    console.log('Proposal Name :  '+ethers.utils.parseBytes32String(proposal.name)+''+' Proposal Vote Count :  '+proposal.voteCount);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
