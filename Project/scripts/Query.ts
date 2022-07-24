d1feaebe3f5adb41d1a207a3cbc93ded1c1d2885
Muhammad Hammad Mobin <muhammadhammadmobin@gmail.com>
2022-07-25 02:47:37 +0500

    Add img and some changes

--- Project/scripts/Query.ts
+++ Project/scripts/Query.ts
@@ -8,7 +8,22 @@ import { CustomBallot } from "../typechain";
 // Do never expose your keys like this
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
-
+  function setupProvider() {
+    const infuraOptions = process.env.INFURA_API_KEY
+      ? process.env.INFURA_API_SECRET
+        ? {
+            projectId: process.env.INFURA_API_KEY,
+            projectSecret: process.env.INFURA_API_SECRET,
+          }
+        : process.env.INFURA_API_KEY
+      : "";
+    const options = {
+      alchemy: process.env.ALCHEMY_API_KEY,
+      infura: infuraOptions,
+    };
+    const provider = ethers.providers.getDefaultProvider("goerli", options);
+    return provider;
+  }
 
 async function main() {
   const wallet =
@@ -16,7 +31,7 @@ async function main() {
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
       : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
   console.log(`Using address ${wallet.address}`);
-  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const provider = setupProvider();
   const signer = wallet.connect(provider);
   const balanceBN = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBN));
@@ -43,7 +58,7 @@ const customBallotContract: CustomBallot = new Contract(
 // Call the winnerName function from the ballot contract
 const winningProposalName = await customBallotContract.winnerName();
 //Display the winning proposal. Make sure to convert bytes32 to string 
-console.log ("And the winning proposal is..." +  + ethers.utils.parseBytes32String(winningProposalName))
+console.log ("And the winning proposal is..."+ ethers.utils.parseBytes32String(winningProposalName))
 
 }
 
