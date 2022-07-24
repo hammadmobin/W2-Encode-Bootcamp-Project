1ba5e03e54dceeba24288b5913344d5add30ee18
udibgui <70275038+udibgui@users.noreply.github.com>
2022-07-24 17:55:55 +0300

    Add files via upload

--- Project/scripts/Query.ts
+++ Project/scripts/Query.ts
@@ -10,31 +10,12 @@ const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
 
-function setupProvider() {
-  const infuraOptions = process.env.INFURA_API_KEY
-    ? process.env.INFURA_API_SECRET
-      ? {
-          projectId: process.env.INFURA_API_KEY,
-          projectSecret: process.env.INFURA_API_SECRET,
-        }
-      : process.env.INFURA_API_KEY
-    : "";
-  const options = {
-    alchemy: process.env.ALCHEMY_API_KEY,
-    infura: infuraOptions,
-    etherscan : process.env.ETHERSCAN_API_KEY,
-  };
-  const provider = ethers.providers.getDefaultProvider("ropsten", options);
-  return provider;
-}
-
 async function main() {
   const wallet =
     process.env.MNEMONIC && process.env.MNEMONIC.length > 0
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
       : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
   console.log(`Using address ${wallet.address}`);
-  //const provider = setupProvider();
   const provider = ethers.providers.getDefaultProvider("ropsten");
   const signer = wallet.connect(provider);
   const balanceBN = await signer.getBalance();
